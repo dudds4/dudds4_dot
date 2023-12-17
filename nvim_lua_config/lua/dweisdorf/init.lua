0e1221c23af08634f791e382987f58667b2919ab
David Weisdorf <weisdorf.david@gmail.com>
2023-12-17 11:49:20 +0200

    add text highlight on yank

--- nvim_lua_config/lua/dweisdorf/init.lua
+++ nvim_lua_config/lua/dweisdorf/init.lua
@@ -4,6 +4,14 @@ require("dweisdorf.keymaps")
 
 ---- *other* configs ----
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight_yank', {}),
+    desc = 'Highlight selection on yank',
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 50 }
+    end
+})
 
 -- removes trailing whitespace on buffer write.
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
